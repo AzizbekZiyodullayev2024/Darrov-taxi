10386f7aa78aa4881f3bac5b2a54a44f3f15bf7d
AzizbekZiyodullayev2024 <ziyodulayeva9@gmail.com>
2025-05-05 16:35:38 +0500

    Initial

--- routes/api.php
+++ routes/api.php
@@ -143,7 +143,6 @@ Route::post('/client/verify', [AuthController::class, 'verifyClient']);
 
 Route::post('/receive-status', [AuthController::class, 'receive'])->name('receive_status');
 
-
 /* Any route */
 Route::any('/{all?}', function () {
     return response()->json([
